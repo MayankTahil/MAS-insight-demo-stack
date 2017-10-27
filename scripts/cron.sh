072dcdecab6aa87518bd5e88b1f1260874122812
MayankTahil <mayank.tahil@gmail.com>
2017-10-26 21:21:19 -0700

    test

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,22 +1,22 @@
-!#/bin/bash 
+#!/bin/bash 
 
 cd /MASaaS
 
 # Tear down the stack
-docker-compse -f mas-insight.stack.yaml down 
+docker-compose -f mas-insight.stack.yaml down 
 
 # Update code from repository and update images
 git pull 
 docker-compose -f ns-init.yaml pull 
-docker-compse -f mas-insight.stack.yaml pull
+docker-compose -f mas-insight.stack.yaml pull
 
 # Wait for a bit
 sleep 5
 
-docker-compse -f ns-init.yaml up -d
+docker-compose -f ns-init.yaml up -d
 
 sleep 45
 
 # Restart the stack
 source ~/.bashrc
-docker-compse -f mas-insight.stack.yaml up -d
+docker-compose -f mas-insight.stack.yaml up -d
